76262b1f8de2d35af248c91079c0d9c3bebec7c9
Théo Dupuis <theo@squadracer.com>
2021-12-08 09:35:38 +0100

    refacto day07

--- days/day07/day07.rb
+++ days/day07/day07.rb
@@ -5,15 +5,22 @@ module Day07
 
   def self.part1(input)
     crabs = parse(input).sort
-    objective = crabs[crabs.size/2]
-    crabs.sum{|crab| (crab - objective).abs}
+    target = crabs[crabs.size / 2]
+    crabs.sum { |crab| (crab - target).abs }
+  end
+
+  def self.fuel_consumed(crabs, target)
+    crabs.sum do |crab|
+      n = (crab - target).abs
+      (n * n + n) / 2
+    end
   end
 
   def self.part2(input)
     crabs = parse(input).sort
     max = crabs.last
-    (0..max).map do |objectif|
-      crabs.sum{|crab|n = (crab-objectif).abs; (n*n+n)/2}
+    (0..max).map do |target|
+      fuel_consumed(crabs, target)
     end
   end
 
@@ -21,20 +28,16 @@ module Day07
     crabs = parse(input).sort
     min = 0
     max = crabs.max
-    # index = 472
     while min < max
-      med = (max+min)/2
-      p [min, med, max]
-      left = crabs.sum{|crab|n = (crab-med).abs; (n*n+n)/2}
-      right = crabs.sum{|crab|n = (crab-med-1).abs; (n*n+n)/2}
-      p [left, right]
+      med = (max + min) / 2
+      left = fuel_consumed(crabs, med)
+      right = fuel_consumed(crabs, med + 1)
       if left >= right
         min = med + 1
       else
-        max= med
+        max = med
       end
     end
-    p [min,max]
-    [crabs.sum{|crab|n = (crab-min).abs; (n*n+n)/2}, crabs.sum{|crab|n = (crab-max).abs; (n*n+n)/2}].min
+    fuel_consumed(crabs, max)
   end
 end
